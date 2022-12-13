c75fbe101584253d9377264ee2a2596b91e09ea0
Amber Murphy <45693571+ajm-kohaku@users.noreply.github.com>
2022-12-12 19:14:52 -0500

    update project after pylint

--- project-two/TextBasedGame.py
+++ project-two/TextBasedGame.py
@@ -1,7 +1,7 @@
 '''
-Amber Murphy
-IT-140: 22EW22
-Module 7: TextBasedGame
+# Amber Murphy
+# IT-140: 22EW22
+# Module 7: TextBasedGame
 '''
 import collections
 import json
@@ -11,12 +11,13 @@ import time
 import re
 
 
-class gameStats:
+class GameStats:
+    '''Global variables for the game.'''
+
     def __init__(self):
         self.inventory = ['Turtle', 'Dragon', 'Bird', 'Tiger']
         self.tokens = ['Turtle', 'Dragon', 'Bird', 'Tiger']
         self.win_inventory = ['Sword', 'Talisman', 'Manuscript']
-        self.options = ['go', 'place', 'get', 'help', 'intro','exit']
         self.can_win = False
         self.exit = False
         self.start = True
@@ -26,11 +27,13 @@ class gameStats:
         self.game_data = {}
 
 
-gamer = gameStats()
+gamer = GameStats()
 DECORATIVE_TEXT = '.:｡+ﾟﾟ+｡:.ﾟ.:｡+ﾟﾟ+｡:.ﾟ.:｡+ﾟﾟ+｡:.ﾟ.:｡+ﾟﾟ+｡:.ﾟ.:｡+ﾟﾟ+｡:.ﾟ.:｡+ﾟﾟ+｡:.ﾟ.:｡+ﾟﾟ+｡:.ﾟ.:｡+ﾟﾟ+｡:.ﾟ.:｡+ﾟﾟ+｡:.ﾟ.:｡+ﾟﾟ+｡:.ﾟ.:｡+ﾟﾟ+｡:.ﾟ.:｡+ﾟﾟ+｡:.ﾟ.:｡+ﾟﾟ+｡:.ﾟ'
 
 
 def intro_text():
+    '''Create introduction text to the game.'''
+    # pylint: disable=line-too-long
     return ('\n     WELCOME TO ONMYOJI!\n'
             '\n You are a young Onmyoudou practitioner in the late Heian Period of Japan. Time is running out to stop the emergence of the Great Serpent.'
             '\n The city\'s magical array needs to be activated to prevent the Serpent from fully manifesting and destroying the city.'
@@ -43,6 +46,8 @@ def intro_text():
 
 
 def instruction_text():
+    '''Create instruction text for the game.'''
+    # pylint: disable=line-too-long
     return ('\n\nInstructions:'
             '\n To move to a different location, type: "go " followed by direction provided in the options.'
             '\n To place an item at a shrine, type: "place " followed by the name of the item you want to leave behind.'
@@ -54,19 +59,26 @@ def instruction_text():
 
 
 def can_win():
-    can_win = collections.Counter(
+    '''determine if gamer can win the game right now
+        * update gamer.can_win
+        * print out text if the player hasn't seen it yet.
+    '''
+    win_is_met = collections.Counter(
         gamer.inventory) == collections.Counter(gamer.win_inventory)
+    # pylint: disable=line-too-long
     can_win_text = 'You have all you need to defeat the Great Serpent. Head to the Palace post-haste!\n'
-    if can_win and not gamer.can_win:
+    if win_is_met and not gamer.can_win:
         typewriter_output(f'{can_win_text}\n')
-    elif can_win and gamer.can_win:
+    elif win_is_met and gamer.can_win:
         print(can_win_text)
-    return can_win
+    return win_is_met
 
 
 def boss_room():
+    '''Handle boss room scenario'''
     typewriter_output(gamer.game_data.get('grandPalace').get('description'))
     if gamer.can_win:
+     # pylint: disable=line-too-long
         game_over_text = ('\nYes! You have come prepared to meet the Great Serpent.'
                           '\nAs the figure approaches, you raise Totsuka-no-Tsurugi in one hand, the Ofuda Talisman in the other hand.'
                           '\nYou chant the ritual words taught to you by Seimei in the manuscript. The Serpent recoils. You feel the strength of the city\'s array empower you.'
@@ -90,18 +102,21 @@ def boss_room():
     gamer.exit = True
 
 
-def exit():
+def exit_game():
+    '''write out game exit and set status to close the game'''
     typewriter_output('Exiting the game. Goodbye!')
     gamer.exit = True
 
 
-def help():
+def game_help():
+    '''reprint game help text. add sleep to keep text up a bit longer'''
     typewriter_output(instruction_text(), 0.01)
     time.sleep(1)
     gamer.has_bad_input = False
 
 
 def move(option: str):
+    '''handle moving to different areas mechanic'''
     directions: dict = gamer.game_data.get(
         gamer.current_room).get('directions')
     if option.lower() in directions.keys():
@@ -112,48 +127,66 @@ def move(option: str):
 
 
 def get_item(item: str):
+    '''handle getting an item mechanic'''
     item = item.title()
     current_room: dict = gamer.game_data.get(gamer.current_room)
-    if current_room.get('item') and current_room.get('item') == item and current_room.get('item') not in gamer.inventory:
+    if (current_room.get('item') and current_room.get('item') == item
+            and current_room.get('item') not in gamer.inventory):
+
         typewriter_output(current_room.get('itemDescription'))
         gamer.inventory.append(current_room.get('item'))
         gamer.game_data.get(gamer.current_room)['item'] = ''
         gamer.has_bad_input = False
-    elif current_room.get('item') and current_room.get('item') == item and current_room.get('item') in gamer.inventory:
-        typewriter_output(
+
+    elif (current_room.get('item') and current_room.get('item') == item
+            and current_room.get('item') in gamer.inventory):
+        # pylint: disable=line-too-long
+        oops_text = (
             f'Well that\'s embarrassing. It looks like {item} is already in your inventory. Let\'s fix that up... there.. all set!')
+        typewriter_output(oops_text)
         gamer.game_data.get(gamer.current_room)['item'] = ''
         gamer.has_bad_input = False
+
     else:
         gamer.has_bad_input = True
 
 
 def place_token(item: str):
+    '''handle placing a token mechanic'''
     current_room = gamer.game_data.get(gamer.current_room)
-    if current_room.get('token') and current_room.get('token') == item.title() and current_room.get('token') in gamer.inventory:
+    if (current_room.get('token') and current_room.get('token') == item.title()
+            and current_room.get('token') in gamer.inventory):
+
         typewriter_output(current_room.get('tokenDescription'))
         gamer.inventory.remove(item.title())
         gamer.game_data.get(gamer.current_room)['item'] = item.title()
         gamer.has_bad_input = False
-    elif current_room.get('token') and current_room.get('token') == item.title() and current_room.get('token') not in gamer.inventory:
+
+    elif (current_room.get('token') and current_room.get('token') == item.title()
+            and current_room.get('token') not in gamer.inventory):
+
         typewriter_output(f'The {item} is not currently in your inventory')
         gamer.has_bad_input = False
+
     elif current_room.get('token') and current_room.get('token') in gamer.inventory:
         typewriter_output(current_room.get('wrongTokenDescription'))
         gamer.inventory.remove(item.title())
         gamer.game_data.get(gamer.current_room)['item'] = item.title()
         gamer.has_bad_input = False
+
     else:
         gamer.has_bad_input = True
 
 
 def print_gamer_inventory():
+    '''print out the current inventory'''
     print('\n\n  Your current inventory:'
           f'\n  {gamer.inventory}'
           '\n')
 
 
 def current_room_options(room):
+    '''return a list of options for the current room'''
     room_dict = gamer.game_data.get(room)
     options = ['']
     for direction in room_dict.get('directions').keys():
@@ -162,81 +195,67 @@ def current_room_options(room):
         options.append(f'Place {room_dict.get("token")}')
     if room_dict.get('item'):
         options.append(f'Get {room_dict.get("item")}')
-    options.append('Intro (you may need to scroll up to see the text repeated)')
+    options.append(
+        'Intro (you may need to scroll up to see the text repeated)')
     options.append('Help')
     options.append('Exit')
     return '\n\t\t'.join(options)
 
 
 def room_text(room: str):
+    '''return the text for the current room'''
     gamer.can_win = can_win()
-    room_text = ' '
+    text = ' '
     if gamer.start:
-        room_text = (f'You begin at the {get_pretty_name(room)}'
-                    f'\n{gamer.game_data.get(room).get("startDescription")}')
+        text = (f'You begin at the {get_pretty_name(room)}'
+                f'\n{gamer.game_data.get(room).get("startDescription")}')
         gamer.start = False
     else:
-        room_text = (f'You have arrived at the {get_pretty_name(room)}.'
-                     f'\n{gamer.game_data.get(gamer.current_room).get("description")}')
+        text = (f'You have arrived at the {get_pretty_name(room)}.'
+                f'\n{gamer.game_data.get(gamer.current_room).get("description")}')
     if gamer.current_room != 'grandPalace':
-        room_text = (f'{room_text}'
-                     '\nWhat would you like to do?\nYour options are:'
-                     f'{current_room_options(room)}\n')
+        text = (f'{text}'
+                '\nWhat would you like to do?\nYour options are:'
+                f'{current_room_options(room)}\n')
     if gamer.has_bad_input:
-        room_text = f'{room_text}\n Invalid option. Try again.\n\n'
+        text = f'{text}\n Invalid option. Try again.\n\n'
 
-    return f'{room_text}\n*> '
+    return f'{text}\n*> '
 
 
 def invalid_option(option: str):
+    '''handle invalid option'''
     print(f'[{option}] is an invalid option. please try again')
 
 
 def get_pretty_name(text: str):
+    '''return a pretty name for the room'''
     name = text[0].upper() + text[1:]
     name_list = re.findall('[A-Z][^A-Z]*', name)
     return ' '.join(name_list)
 
 
-def create_key_name(text: str):
-    name = ''.join(text.split())
-    name_list = [name[0].lower(), name[1:]]
-    return ''.join(name_list)
-
-
 def load_game_data():
+    '''load the game data from the json file'''
     filename = 'game-data.json'
     if not os.path.exists(filename):
-        print('File {} does not exist.'.format(filename))
+        print(f'File: {filename} does not exist.')
         sys.exit(1)
 
-    with open(filename) as json_file:
+    with open(filename, encoding="utf-8") as json_file:
         return json.load(json_file)
 
 
 def typewriter_output(text, speed=0.05):
+    '''print out text with a typewriter effect'''
     for char in text:
         sys.stdout.write(char)
         sys.stdout.flush()
         time.sleep(speed)
 
 
-def parse_gamer_input(input: str):
-    split_string = input.lower().split()
-    result = {'option': '', 'choice': ''}
-    if len(split_string) == 1:
-        result['option'] = split_string[0]
-        result['choice'] = split_string[0]
-    elif len(split_string) > 1:
-        result['option'] = split_string[0]
-        result['choice'] = ''.join(split_string[1:])
-    else:
-        result['option'] = ' '
-        result['choice'] = ' '
-    return result
-
-
 def main():
+    '''main function'''
     gamer.game_data = load_game_data()
     typewriter_output(intro_text())
     print(DECORATIVE_TEXT)
@@ -245,24 +264,25 @@ def main():
     while gamer.exit is False:
         print_gamer_inventory()
         gamer_input = str(input(room_text(gamer.current_room)))
-        option = parse_gamer_input(gamer_input).get('option')
-        choice = parse_gamer_input(gamer_input).get('choice')
+        gamer_input = gamer_input.lower().split()
 
-        match option:
-            case 'go':
-                move(choice)
-            case 'place':
-                place_token(choice)
-            case 'get':
-                get_item(choice)
-            case 'help':
-                help()
-            case 'intro':
-                print(intro_text())
-            case 'exit':
-                exit()
-            case _:
-                gamer.has_bad_input = True
+        if gamer_input and gamer_input[0] == 'go' and len(gamer_input) == 2:
+            move(gamer_input[1])
+        elif gamer_input and gamer_input[0] == 'place' and len(gamer_input) == 2:
+            place_token(gamer_input[1])
+        elif gamer_input and gamer_input[0] == 'get' and len(gamer_input) == 2:
+            get_item(gamer_input[1])
+        elif gamer_input and gamer_input[0] == 'help':
+            typewriter_output(instruction_text(), speed=0.01)
+            time.sleep(1)
+        elif gamer_input and gamer_input[0] == 'intro':
+            typewriter_output(intro_text(),speed=0.01)
+            time.sleep(1)
+        elif gamer_input and gamer_input[0] == 'exit':
+            exit_game()
+        else:
+            gamer.has_bad_input = True
+
         if gamer.current_room == 'grandPalace':
             boss_room()
 
